3a216f4cfb50ed139f8fddb7fc98cb439cc8190e
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-09-16 20:22:46 -0500

    visitantes
    Visitantes

--- .history/app_20190916182218.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-
-mongoose.connection.on("error", function(e) { console.error(e); });
-
-app.listen(3000, () => console.log("Listening on port 3000 ..."));
